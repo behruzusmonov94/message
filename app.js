d20c0ff10c68e30cce807fd0fc2ae0fc4f90eed8
ubehruz <behruzusmonov94@gmail.com>
2021-04-16 18:35:13 +0500

    gsap animation qushildi

--- app.js
+++ app.js
@@ -38,5 +38,10 @@ for(let i = 0; i < localStorage.length; i++){
     messages.innerHTML += html
 
     
-
 }
+
+
+gsap.from('.messages li', {x: 30, opacity: 0, duration: 0.3, stagger: 0.1, delay: 0.3, ease: "back"})
+gsap.from('h1', {y: -30, opacity: 0, duration: 0.5, stagger: 0.1, ease: "ease"})
+gsap.from('hr', {width: 0,  duration: 0.5, stagger: 0.1, ease: "linear"})
+gsap.from('#messForm', {y: 100,  duration: 0.5, stagger: 1, delay: 0.8, ease: "back"})
